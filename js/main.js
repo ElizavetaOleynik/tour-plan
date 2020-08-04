467692b7e64c03642ac54048caa5bc3a8e0c6a9b
ElizavetaOleynik <oleynik.elizaveta1995@gmail.com>
2020-08-04 17:03:33 +0300

    added modal windows

--- js/main.js
+++ js/main.js
@@ -1,31 +1,72 @@
-var hotelSlider = new Swiper('.hotel-slider', {
-	// Optional parameters
-	loop: true,
+$(document).ready(function () {
+	var hotelSlider = new Swiper('.hotel-slider', {
+		// Optional parameters
+		loop: true,
 
 
-	// Navigation arrows
-	navigation: {
-		nextEl: '.hotel-slider__button--next',
-		prevEl: '.hotel-slider__button--prev',
-	},
+		// Navigation arrows
+		navigation: {
+			nextEl: '.hotel-slider__button--next',
+			prevEl: '.hotel-slider__button--prev',
+		},
 
-})
+	})
 
-var reviewsSlider = new Swiper('.reviews-slider', {
-	// Optional parameters
-	loop: true,
+	var reviewsSlider = new Swiper('.reviews-slider', {
+		// Optional parameters
+		loop: true,
 
 
-	// Navigation arrows
-	navigation: {
-		nextEl: '.reviews-slider__button--next',
-		prevEl: '.reviews-slider__button--prev',
-	},
+		// Navigation arrows
+		navigation: {
+			nextEl: '.reviews-slider__button--next',
+			prevEl: '.reviews-slider__button--prev',
+		},
 
-})
+	})
 
-var menuButton = document.querySelector('.menu-burger');
-menuButton.addEventListener('click',function () {
-	console.log('Клик');
-	document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible')
-})
+	var menuButton = document.querySelector('.menu-burger');
+	menuButton.addEventListener('click', function () {
+		console.log('Клик');
+		document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible')
+	});
+
+
+
+
+	var modalButton = $("[data-toggle=modal]");
+	var closeModalButton = $(".modal__close");
+	modalButton.on('click', openModal);
+	closeModalButton.on('click', closeModal);
+
+
+	function openModal() {
+		var targetModal = $(this).attr('data-href');
+		$(targetModal).find('.modal__overlay').addClass("modal__overlay--visible");
+		$(targetModal).find('.modal__dialog').addClass("modal__dialog--visible");
+	};
+
+	function closeModal(event) {
+		event.preventDefault();
+		var modalOverlay = $('.modal__overlay');
+		var modalDialog = $('.modal__dialog');
+		modalOverlay.removeClass('modal__overlay--visible');
+		modalDialog.removeClass('modal__dialog--visible');
+	};
+
+
+	$(window).keydown(function (e) {
+		// ESCAPE key pressed
+		if (e.keyCode == 27) {
+			event.preventDefault();
+			var modalOverlay = $('.modal__overlay');
+			var modalDialog = $('.modal__dialog');
+			modalOverlay.removeClass('modal__overlay--visible');
+			modalDialog.removeClass('modal__dialog--visible');
+		}
+	});
+
+
+
+
+});
